e5d97f43e96ca07ca6450d0ef5b061424d82a6d7
Chebichii-Lab <natasha.jebichii@student.moringaschool.com>
2021-06-19 18:07:05 +0300

    create comment function in the views.py file

--- app/main/views.py
+++ app/main/views.py
@@ -19,3 +19,24 @@ def index():
         db.session.commit()
         mail_message("Thank you for subscribing to BLOG IT!", "email/welcome", new_sub.email)
     return render_template("index.html",blogs= blogs,quote = quote)
+
+
+@main.route("/blog/<int:id>", methods = ["POST", "GET"])
+def blog(id):
+    blog = Blog.query.filter_by(id = id).first()
+    comments = Comment.query.filter_by(blog_id = id).all()
+    comment_form = CommentForm()
+    comment_count = len(comments)
+
+    if comment_form.validate_on_submit():
+        comment = comment_form.comment.data
+        comment_form.comment.data = ""
+        comment_alias = comment_form.alias.data
+        comment_form.alias.data = ""
+        if current_user.is_authenticated:
+            comment_alias = current_user.username
+        new_comment = Comment(comment = comment, comment_at = datetime.now(),comment_by = comment_alias, blog_id = id)
+        new_comment.save_comment()
+        return redirect(url_for("main.blog", id = blog.id))
+
+    return render_template("post.html",blog = blog,comments = comments,comment_form = comment_form,comment_count = comment_count)
