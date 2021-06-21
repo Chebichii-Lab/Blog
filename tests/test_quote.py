367e9efb9fde4f36e21ac859d6bb0ef339a0c08b
Chebichii-Lab <natasha.jebichii@student.moringaschool.com>
2021-06-21 10:55:32 +0300

    run test of class Quote

---
+++ tests/test_quote.py
@@ -0,0 +1,16 @@
+import unittest
+from app.models import Quote
+
+class TestQuote(unittest.TestCase):
+    def setUp(self):
+        """
+        Set up method that will run before every Test
+        """
+        self.random_quote = Quote("Natasha Chebichii", "Run the world")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.random_quote, Quote))
+
+    def test_init(self):
+        self.assertEqual(self.random_quote.author, "Natasha Chebichii")
+        self.assertEqual(self.random_quote.quote,"Run the world")
